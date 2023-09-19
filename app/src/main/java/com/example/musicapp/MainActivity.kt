232b73a0d06789d8ba84915f508cbe96c540fa05
Milena <milena.cuellarpa@elorrieta-errekamari.com>
2023-09-19 19:27:16 +0200

    codigo boton pause/play invisible

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.musicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imagePlay = findViewById<ImageView>(R.id.imageViewPlay)
+        val imagePause = findViewById<ImageView>(R.id.imageViewPause)
+
+        imagePlay.setOnClickListener(View.OnClickListener {
+            imagePlay.visibility=View.GONE
+            imagePause.visibility= View.VISIBLE
+        })
+        imagePause.setOnClickListener(View.OnClickListener {
+            imagePlay.visibility =View.VISIBLE
+            imagePause.visibility =View.GONE
+        })
     }
 }
